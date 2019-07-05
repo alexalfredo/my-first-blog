904639de0f4bac87c76b139fb20c6dda44185480
Alex Alfredo <webalx@gmail.com>
2019-07-05 17:15:12 -0300

    add the URLs in ALLOWER_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't38y#r7%7lxcpwu9+y6+_3@7-%1v+$n^ky-d!@hq!ln=t!az4q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','localhost']
 
 
 # Application definition
